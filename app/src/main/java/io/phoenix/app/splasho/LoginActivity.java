05ce8f838d0ef77d752d4592860b323f3c9c8de2
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-10-21 04:00:24 -0400

    LoginActivity logic to check shared preferences

--- app/src/main/java/io/phoenix/app/splasho/LoginActivity.java
+++ app/src/main/java/io/phoenix/app/splasho/LoginActivity.java
@@ -1,7 +1,9 @@
 package io.phoenix.app.splasho;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,10 +13,23 @@ import android.view.View;
  */
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String CONSUMER_TOKEN = "consumer_token";
+    private static final String SKIP_LOGIN = "skip_login";
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
+
+        if (mSharedPreferences.getBoolean(SKIP_LOGIN, false) ||
+                mSharedPreferences.getString(CONSUMER_TOKEN, null) != null) {
+            skipLoginScreen();
+        }
     }
 
     public void onLoginWithUnsplash(View view) {
@@ -22,6 +37,12 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void onSkipLogin(View view) {
+        mEditor.putBoolean(SKIP_LOGIN, true);
+        mEditor.commit();
+        skipLoginScreen();
+    }
+
+    private void skipLoginScreen() {
         Intent mainIntent = new Intent(this, MainActivity.class);
         startActivity(mainIntent);
         finish();
