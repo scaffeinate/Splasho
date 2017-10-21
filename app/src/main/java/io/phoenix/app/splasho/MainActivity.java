cf2a57a425b519baa466aa157b684145f229f3be
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-10-21 02:37:50 -0400

    Add user profile to the nav drawer header view

--- app/src/main/java/io/phoenix/app/splasho/MainActivity.java
+++ app/src/main/java/io/phoenix/app/splasho/MainActivity.java
@@ -3,18 +3,26 @@ package io.phoenix.app.splasho;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String SELECTED_POSITION = "selected_position";
 
     private Toolbar mToolbar;
     private DrawerLayout mDrawerLayout;
     private NavigationView mNavigationView;
+
+    private int selectedPosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +31,57 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         mToolbar = findViewById(R.id.toolbar);
         mDrawerLayout = findViewById(R.id.drawer_layout);
+        mNavigationView = findViewById(R.id.navigation_view);
 
         setSupportActionBar(mToolbar);
+        setupToolbar();
 
+        if (savedInstanceState != null) {
+            selectedPosition = savedInstanceState.getInt(SELECTED_POSITION, 0);
+        }
+
+        mNavigationView.setNavigationItemSelectedListener(this);
+        Menu menu = mNavigationView.getMenu();
+        if (menu != null) {
+            onNavigationItemSelected(menu.getItem(selectedPosition));
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        item.setChecked(true);
+        switch (item.getItemId()) {
+            case R.id.action_photos:
+                selectedPosition = 0;
+                Toast.makeText(this, "Photos", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_collections:
+                selectedPosition = 1;
+                Toast.makeText(this, "Collections", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_settings:
+                selectedPosition = 2;
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(SELECTED_POSITION, selectedPosition);
+    }
+
+    private void setupToolbar() {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowTitleEnabled(false);
         actionBar.setDisplayHomeAsUpEnabled(true);
@@ -33,17 +89,5 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
-
-        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
-        mNavigationView.setNavigationItemSelectedListener(this);
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        item.setChecked(true);
-        switch (item.getItemId()) {
-            
-        }
-        return true;
     }
 }
