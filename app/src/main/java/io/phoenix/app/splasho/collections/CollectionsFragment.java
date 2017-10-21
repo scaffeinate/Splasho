c6edb272f80a39c7328e560c237c3d818d1ad101
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-10-21 16:00:36 -0400

    Add PhotosFragment and CollectionsFragment

---
+++ app/src/main/java/io/phoenix/app/splasho/collections/CollectionsFragment.java
@@ -0,0 +1,8 @@
+package io.phoenix.app.splasho.collections;
+
+/**
+ * Created by sudharti on 10/21/17.
+ */
+
+public class CollectionsFragment {
+}
---
+++ app/src/main/java/io/phoenix/app/splasho/photos/PhotosFragment.java
@@ -0,0 +1,27 @@
+package io.phoenix.app.splasho.photos;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.phoenix.app.splasho.R;
+
+/**
+ * Created by sudharti on 10/21/17.
+ */
+
+public class PhotosFragment extends Fragment {
+    public static PhotosFragment newInstance() {
+        return new PhotosFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_photos, container, false);
+        return view;
+    }
+}
