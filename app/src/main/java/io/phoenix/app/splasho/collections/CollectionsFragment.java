c25d74e61bb4da1cbcf6817333cfe1bcb57458d2
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-10-21 16:00:54 -0400

    Add ViewPager adapter for Photos

--- app/src/main/java/io/phoenix/app/splasho/collections/CollectionsFragment.java
+++ app/src/main/java/io/phoenix/app/splasho/collections/CollectionsFragment.java
@@ -1,8 +1,27 @@
 package io.phoenix.app.splasho.collections;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.phoenix.app.splasho.R;
+
 /**
  * Created by sudharti on 10/21/17.
  */
 
-public class CollectionsFragment {
+public class CollectionsFragment extends Fragment {
+    public static CollectionsFragment newInstance() {
+        return new CollectionsFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_collections, container, false);
+        return view;
+    }
 }
